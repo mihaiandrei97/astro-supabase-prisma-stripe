e262bfc46788b5c6722b1e4dc11baca87295c770
Mihai Adrian Andrei <mihai.andrei1597@gmail.com>
2024-01-23 16:33:49 +0200

    added shadcn + pricing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  })]
 });
---
+++ src/lib/products.ts
@@ -0,0 +1,63 @@
+export type Product = {
+    id: string,
+    name: string;
+    description: string;
+    price: number;
+    color: string;
+    benefits: { text: string; available: boolean }[];
+    role: string;
+};
+
+export const products: Product[] = [
+    {
+      id: "1",
+      name: "Basic",
+      description: "Basic product with essential features",
+      price: 2990,
+      color: "#804edd",
+      benefits: [
+        { text: "Access to basic features", available: true },
+        { text: "24/7 customer support", available: true },
+        { text: "Exclusive discounts", available: false },
+        { text: "Priority customer support", available: false },
+        { text: "Early access to new releases", available: false },
+        { text: "VIP customer support", available: false },
+        { text: "Personalized recommendations", available: false },
+      ],
+      role: "pro_basic",
+    },
+    {
+      id: "2",
+      name: "Gold",
+      description: "Premium product with additional features",
+      price: 5999,
+      color: "#eab308",
+      benefits: [
+        { text: "Access to basic features", available: true },
+        { text: "24/7 customer support", available: true },
+        { text: "Exclusive discounts", available: true },
+        { text: "Priority customer support", available: true },
+        { text: "Early access to new releases", available: false },
+        { text: "VIP customer support", available: false },
+        { text: "Personalized recommendations", available: false },
+      ],
+      role: "pro_gold",
+    },
+    {
+      id: "3",
+      name: "Platinum",
+      description: "Top-tier product with exclusive features",
+      price: 9999,
+      color: "#3b82f6",
+      benefits: [
+        { text: "Access to basic features", available: true },
+        { text: "24/7 customer support", available: true },
+        { text: "Exclusive discounts", available: true },
+        { text: "Priority customer support", available: true },
+        { text: "Early access to new releases", available: true },
+        { text: "VIP customer support", available: true },
+        { text: "Personalized recommendations", available: true },
+      ],
+      role: "pro_platinum",
+    },
+  ];
