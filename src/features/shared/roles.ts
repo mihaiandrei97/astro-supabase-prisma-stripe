f9a7cd626ef6a7b97e90f0ce2f5d28f621bc6d3e
Mihai Adrian Andrei <mihai.andrei1597@gmail.com>
2024-02-14 15:14:15 +0200

    added roles

--- src/features/shared/roles.ts
+++ src/features/shared/roles.ts
@@ -1,5 +1,13 @@
-import type { Role } from "@prisma/client";
+import type { Role, ProTier } from "@prisma/client";
 
 export function userIsAdmin(user: {role: Role} | undefined) {
     return user?.role === "ADMIN";
+}
+
+export function userIsPro(user: {proTier: ProTier} | undefined) {
+    return !!user?.proTier;
+}
+
+export function userIsProTier(user: {proTier: ProTier} | undefined, tier: ProTier) {
+    return user?.proTier === tier;
 }
